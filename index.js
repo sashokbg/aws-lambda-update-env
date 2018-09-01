8f7805de3f546ac8ed82c2b5ffcdb4f7a106aff0
Alexander KIRILOV <alexander.n.kirilov@gmail.com>
2018-09-01 12:41:51 +0200

    Moved arg parse to lib and refactored methods

--- index.js
+++ index.js
@@ -1,76 +1,78 @@
-const argv = require('minimist')(process.argv.slice(1));
-
 process.env.AWS_SDK_LOAD_CONFIG=1;
+var colors = require('colors');
 
+const { parse_args } = require('./lib/argumentsParser.js'); 
 const AWS = require('aws-sdk');
 const { spawn } = require('child_process');
-
-let key;
-let value;
-let stack;
 
 var lambda = new AWS.Lambda({apiVersion: '2015-03-31'});
 var cloudformation = new AWS.CloudFormation();
 
-let functions = [];
 
-function print_usage(){
-  console.info('Usage: update-lambda-env {variable name} {new variable value} --stackName myStack');
-}
+function update_lambdas(functions, options){
+  console.log('Found the following functions to update:\n');
+  console.dir(functions);
 
-function parse_args(){
-  if((!argv.s && !argv.stackName) || (typeof argv.s != 'string' && typeof argv.stackName != 'string')){
-    console.error('Please specify the stack name using -s or --stackName');
-  } else {
-    stack = argv.s || argv.stackName;
-  }
-
-  if(argv._.length < 3){
-    console.error('ERROR: Too few args.');
-    print_usage();
-    return;
-  }
-
-  key = argv._[1];
-  value = argv._[2];
-  stack = stack;
-
-  console.log(`Updating Key [${key}] with new value [${value}] on Stack [${stack}]`);
-}
-
-function update_lambdas(){
-  lambda.getFunctionConfiguration({FunctionName: 'createJobs-test'}, (err, data) =>{
-    if(err){
-      console.err(err);
-      return;
-    } else {
-      console.log(data.Environment.Variables);
-      let currentEnv = data.Environment;
-      currentEnv.Variables[key] = value;
-      lambda.updateFunctionConfiguration({FunctionName: 'createJobs-test', Environment: currentEnv}, (err, data) =>{
-        if(err){
-          console.error(err);
+  for(functionName of functions){
+    load_lambda_env(functionName)
+      .then((data) => {
+        if(data.functionEnv[options.key]){
+          console.log('UPDATING'.green+`: Function [${data.functionName}]`);
         } else {
-          console.log(data);
-        }
+          console.log('SKIPPING'.yellow+`: Function [${data.functionName}] does not contain key [${options.key}].`);
+        } 
+      })
+      .catch(err => {
+        console.error(`Problem loading function configuration for [${err.functionName}]`,err);
       });
-    }
+  }
+
+}
+
+function load_lambda_env(functionName){
+  console.log(`Loading function [${functionName}]`);
+  return new Promise((resolve, reject) =>{
+    lambda.getFunctionConfiguration({FunctionName: functionName}, (err, data) =>{
+      if(err){
+        err.functionName = functionName;
+        reject(err);
+      } else {
+        resolve({
+          functionName: functionName,
+          functionEnv: data.Environment.Variables});
+        //let currentEnv = data.Environment;
+        //currentEnv.Variables[key] = value;
+        //lambda.updateFunctionConfiguration({FunctionName: func, Environment: currentEnv}, (err, data) =>{
+        //  if(err){
+        //    console.error(err);
+        //  } else {
+        //    console.log(data);
+        //  }
+        //});
+      }
+    });
   });
 }
 
 function find_lambdas(){
-  cloudformation.listStackResources({StackName: stack}, function(err, data) {
-    if (err){
-      console.log(err, err.stack);
-      return;
-    } else {
-      var functions = data.StackResourceSummaries.filter(resource => resource.ResourceType==='AWS::Lambda::Function').map(funct => funct.PhysicalResourceId);
-      console.log('Found the following functions to update:\n');
-      console.dir(functions);
-      update_lambdas();
-    }
+  console.log(`Updating Key [${this.key}] with new value [${this.value}] on Stack [${this.stack}]`);
+  let parent = this;
+
+  return new Promise(function(resolve, reject){
+    cloudformation.listStackResources({StackName: parent.stack}, function(err, data) {
+      if (err){
+        console.log(err, err.stack);
+        reject(err);
+        return;
+      } else {
+        let functions = data.StackResourceSummaries.filter(resource => resource.ResourceType==='AWS::Lambda::Function').map(funct => funct.PhysicalResourceId);
+        resolve(functions);
+      }
+    });
   });
 }
 
-parse_args();
-find_lambdas();
+let options = parse_args();
+find_lambdas.call(options)
+  .then((functions)=>update_lambdas(functions, options))
+  .catch(err=> console.error(err));
